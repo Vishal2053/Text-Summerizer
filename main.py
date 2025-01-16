1b43fec6ae36baf149fe4e8f16724031fc111995
Vishal More <130221313+Vishal2053@users.noreply.github.com>
2025-01-16 13:35:27 +0530

    Model evaluation added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.textSummarizer.pipeline.stage_4_model_evaluation_pipeline import ModelEvaluationTrainingPipeline
+
 STAGE_NAME = "Data Ingestion stage"
 
 try:
@@ -34,6 +36,20 @@ try:
     logger.info(f"stage {STAGE_NAME} initiated")
     data_ingestion_pipeline = ModelTrainerTrainingPipeline
     data_ingestion_pipeline.initiate_model_trainer()
+    logger.info(f"stage {STAGE_NAME} completed")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Model Evaluation stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline = ModelEvaluationTrainingPipeline()
+    data_ingestion_pipeline.initiate_model_evaluate()
     logger.info(f"stage {STAGE_NAME} completed")
 
 except Exception as e:
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -35,3 +35,12 @@ class ModelTrainerConfig:
 
 
 
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    data_path: Path
+    model_path: Path
+    tokenizer_path: Path
+    metric_file_name: Path
+
+
